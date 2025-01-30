bf1f0641ad005dfe4ac205b288431d6191cbb5b1
aquabarnabas <aquabarnabas@gmail.com>
2025-01-30 16:19:20 -0500

    added changes

---
+++ server/dist/config/connection.js
@@ -0,0 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+mongoose.connect(process.env.MONGODB_URI ||
+    "mongodb+srv://dbUser:<hellomister123>@cluster0.pf0vp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+export default mongoose.connection;
