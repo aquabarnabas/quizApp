fe3d263deb84b73e67a8ed73e4050c2928d41a41
aquabarnabas <aquabarnabas@gmail.com>
2025-01-30 16:42:50 -0500

    pushed changes

--- server/dist/seeds/seed.js
+++ server/dist/seeds/seed.js
@@ -1,10 +1,12 @@
 import db from "../config/connection.js";
 import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
-import pythonQuestions from './pythonQuestions.json' assert { type: "json" };
-db.once('open', async () => {
-    await cleanDB('Question', 'questions');
+import { readFile } from "fs/promises";
+// import pythonQuestions from "./pythonQuestions.json" assert { type: "json" };
+const pythonQuestions = JSON.parse(await readFile(new URL("./pythonQuestions.json", import.meta.url), "utf-8"));
+db.once("open", async () => {
+    await cleanDB("Question", "questions");
     await Question.insertMany(pythonQuestions);
-    console.log('Questions seeded!');
+    console.log("Questions seeded!");
     process.exit(0);
 });
--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-    "mongodb+srv://dbUser:<hellomister123>@cluster0.pf0vp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://dbUser:hellomister123@cluster0.pf0vp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 );
 
 export default mongoose.connection;
